77e7c99a970fa398ae5f479da894790e31ba2982
Pc_escritorio_JoseQ <josegabrielquintana@gmail.com>
2026-04-12 21:42:20 -0500

    Me decidi por usar el API de cosas que hacer aburrido me parece una opcion buena por lo cual empiezo mi proyecto

--- MAIN.py
+++ MAIN.py
@@ -1,22 +1,7 @@
 import requests
 
-base_url = "https://pokeapi.co/api/v2/"
+url = "https://bored-api.appbrewery.com/random"
 
-def poke_info(nombre):
-    url = f"{base_url}/pokemon/{nombre}"
-    response = requests.get(url)
-    # significa 200 que la conexion fue efectiva
-    if response.status_code == 200:
-        pokemon_data = response.json()
-        return pokemon_data
-    else:
-        print(f"Fallo en obtener la informacion {response.status_code}")
+respuesta = requests.get(url)
 
-pokemon_name = "rayquaza"
-poke_dictionary = poke_info(pokemon_name)
-
-if poke_dictionary:
-    print(f"Nombre: {poke_dictionary['name'].capitalize()}")
-    print(f"ID: {poke_dictionary['id']}")
-    print(f"Peso: {poke_dictionary['weight']} kg")
-    print(f"altura: {poke_dictionary['height']} m")
+print(respuesta.json())
