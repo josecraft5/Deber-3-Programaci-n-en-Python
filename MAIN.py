32f24beac8cbbbc385803206a2933defb95ead44
Pc_escritorio_JoseQ <josegabrielquintana@gmail.com>
2026-04-12 21:20:04 -0500

    Vi un video de yotube que me explico como usar la libreria con lo que entendi y puedo aplicarla

--- MAIN.py
+++ MAIN.py
@@ -1,7 +1,22 @@
 import requests
 
-url = "https://example.com/"
-req = requests.get(url)
+base_url = "https://pokeapi.co/api/v2/"
 
-print(req)
-print(req.status_code)
+def poke_info(nombre):
+    url = f"{base_url}/pokemon/{nombre}"
+    response = requests.get(url)
+    # significa 200 que la conexion fue efectiva
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return pokemon_data
+    else:
+        print(f"Fallo en obtener la informacion {response.status_code}")
+
+pokemon_name = "rayquaza"
+poke_dictionary = poke_info(pokemon_name)
+
+if poke_dictionary:
+    print(f"Nombre: {poke_dictionary['name'].capitalize()}")
+    print(f"ID: {poke_dictionary['id']}")
+    print(f"Peso: {poke_dictionary['weight']} kg")
+    print(f"altura: {poke_dictionary['height']} m")
