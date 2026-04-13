a65e6bdcfa818bb85efb2f1e8c1d842a7d92e993
Pc_escritorio_JoseQ <josegabrielquintana@gmail.com>
2026-04-12 22:09:10 -0500

    Aqui decidi meter un traductor de ingles a español de forma que el programa traduce

--- MAIN.py
+++ MAIN.py
@@ -1,7 +1,26 @@
 import requests
 
-url = "https://bored-api.appbrewery.com/random"
+from googletrans import Translator
+traductor = Translator()
+def traducir_texto(texto):
+    try:
+        resultado = traductor.translate(texto, src="en", dest="es")
+        return resultado.text
+    except:
+        return texto  # si falla, muestra el original
+# Aqui me ayudo un poco la IA para saber como traducir con src y dest
 
-respuesta = requests.get(url)
+def peticion_random():
+    url = "https://bored-api.appbrewery.com/random"
+    respuesta = requests.get(url)
+    if respuesta.status_code == 200:
+        bored_data = respuesta.json()
+        return bored_data
+    else: 
+        print(f"Error obteniendo informacion: {respuesta.status_code}")
 
-print(respuesta.json())
+
+bored_dictinary = peticion_random()
+
+if bored_dictinary:
+    print(f"Actividad: {traducir_texto(bored_dictinary['activity'])}")
