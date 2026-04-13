3d4e08bd09edc2ce5324c419d049415d4b2d4b43
Pc_escritorio_JoseQ <josegabrielquintana@gmail.com>
2026-04-12 22:31:54 -0500

    Una version mas avanzada del programa con funciones y traducido aun falta programacion

--- MAIN.py
+++ MAIN.py
@@ -7,8 +7,8 @@ def traducir_texto(texto):
         resultado = traductor.translate(texto, src="en", dest="es")
         return resultado.text
     except:
-        return texto  # si falla, muestra el original
-# Aqui me ayudo un poco la IA para saber como traducir con src y dest
+        return texto  
+# Aqui me ayudo un poco la IA para saber como traducir con src y dest igualmente me permitio entender como funciona el try y except
 
 def peticion_random():
     url = "https://bored-api.appbrewery.com/random"
@@ -22,5 +22,19 @@ def peticion_random():
 
 bored_dictinary = peticion_random()
 
-if bored_dictinary:
-    print(f"Actividad: {traducir_texto(bored_dictinary['activity'])}")
+def imprimir_diccionario(dic, clave, nombre, traducir=False):
+    valor = dic.get(clave)
+
+    if valor:
+        if traducir and isinstance(valor, str):
+            valor = traducir_texto(valor)
+
+        print(f"{nombre}: {valor}")
+
+imprimir_diccionario(bored_dictinary, "activity", "Actividad", True)
+imprimir_diccionario(bored_dictinary, "type", "Tipo", True)
+imprimir_diccionario(bored_dictinary, "participants", "Participantes")
+imprimir_diccionario(bored_dictinary, "price", "Precio")
+imprimir_diccionario(bored_dictinary, "duration", "Duracion")
+imprimir_diccionario(bored_dictinary, "kidFriendly", "Amistoso con Niños")
+print(bored_dictinary)
