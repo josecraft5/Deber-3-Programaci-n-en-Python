51644a7fdbf7ae1c13ef0597818677d5341a90ad
Pc_escritorio_JoseQ <josegabrielquintana@gmail.com>
2026-04-12 23:20:45 -0500

    Se creo un menu para hacer multiples solicituddel programa y adicional se mas claves

--- MAIN.py
+++ MAIN.py
@@ -22,7 +22,7 @@ def peticion_random():
 
 bored_dictinary = peticion_random()
 
-def imprimir_diccionario(dic, clave, nombre, traducir=False):
+def imprimir_diccionario(dic, clave, nombre, traducir=False,):
     valor = dic.get(clave)
 
     if valor:
@@ -31,10 +31,35 @@ def imprimir_diccionario(dic, clave, nombre, traducir=False):
 
         print(f"{nombre}: {valor}")
 
-imprimir_diccionario(bored_dictinary, "activity", "Actividad", True)
-imprimir_diccionario(bored_dictinary, "type", "Tipo", True)
-imprimir_diccionario(bored_dictinary, "participants", "Participantes")
-imprimir_diccionario(bored_dictinary, "price", "Precio")
-imprimir_diccionario(bored_dictinary, "duration", "Duracion")
-imprimir_diccionario(bored_dictinary, "kidFriendly", "Amistoso con Niños")
-print(bored_dictinary)
+def mostrar_programa():
+    imprimir_diccionario(bored_dictinary, "activity", "Actividad", True)
+    imprimir_diccionario(bored_dictinary, "type", "Tipo", True)
+    imprimir_diccionario(bored_dictinary, "participants", "Participantes")
+    imprimir_diccionario(bored_dictinary, "price", "Precio")
+    imprimir_diccionario(bored_dictinary, "availability", "Que tan facil es de realizar en una esacala del 0 al 1")
+    imprimir_diccionario(bored_dictinary, "duration", "Duracion", True)
+    imprimir_diccionario(bored_dictinary, "kidFriendly", "Amistoso con Niños")
+    imprimir_diccionario(bored_dictinary, "link", "Link con informacion Util")
+
+while True:
+    print("\nBienvenido a Cosas que hacer cuando estas aburrido")
+    print("==================")
+    print("       Menu")
+    print("===================")
+    print("1. Actividad Aleatoria")
+    print("2. Salir del programa")
+
+    opcion = input("Seleccione una Opcion: ")
+
+    # Validación (como te piden en clase)
+    while opcion != "1" and opcion != "2":
+        opcion = input("Error, seleccione una opcion correcta (1-2): ")
+
+    if opcion == "1":
+        mostrar_programa()
+
+    elif opcion == "2":
+        print("Saliendo del programa...")
+        break
+    
+
